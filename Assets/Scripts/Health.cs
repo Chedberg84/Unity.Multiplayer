b7bfa0c577aa1368d39424f14132a3dd4887c615
Chris Hedberg <christophhedberg@gmail.com>
2016-10-27 07:47:38 -0500

    Added Respawn when player dies

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -16,20 +16,30 @@ public class Health : NetworkBehaviour
     public void TakeDamage(int amount)
     {
         if (!isServer)
-        {
             return;
-        }
-        
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            currentHealth = maxHealth;
+
+            // called on the Server, but invoked on the Clients
+            RpcRespawn();
         }
     }
 
-    void OnChangeHealth(int health)
+    void OnChangeHealth(int currentHealth)
     {
-        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
     }
 }
